90d5fd1c003a31f1e3095201cdd73e1862098b7e
vaibhav973 <vaibhavwadhwa06@gmail.com>
2024-05-04 15:55:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Flutter App Support Page</h1>
+      <p>This page will help with handling deep links for my Flutter app.</p>
+    </div>
+  );
+}
+
+export default App;
